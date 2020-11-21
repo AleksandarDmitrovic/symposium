c44040ce2e58854f40274b157e17d7ca94480272
ericmcgrandle <ericmcgrandle@gmail.com>
2020-11-21 10:41:45 -0800

    fix code in schema files

--- db/schema/04_participants.sql
+++ db/schema/04_participants.sql
@@ -4,4 +4,4 @@ CREATE TABLE participants (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   conversation_id INTEGER REFERENCES conversations(id) ON DELETE CASCADE
-}
+);
