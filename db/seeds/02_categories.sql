78bebd263c009190356f5c8940784e7bc99215e6
JoshGrant5 <joshgg@icloud.com>
2020-11-21 10:29:39 -0800

    Create fake data seeds for categories

---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories (name) VALUES ('Technology');
+INSERT INTO categories (name) VALUES ('Arts');
+INSERT INTO categories (name) VALUES ('Business');
+INSERT INTO categories (name) VALUES ('Comedy');
+INSERT INTO categories (name) VALUES ('Education');
+INSERT INTO categories (name) VALUES ('Science');
+INSERT INTO categories (name) VALUES ('Sports');
+INSERT INTO categories (name) VALUES ('True Crime');
+INSERT INTO categories (name) VALUES ('History');
