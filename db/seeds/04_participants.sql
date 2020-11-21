3c1132605f2700aaf89d4b81f3e76c792251ee85
JoshGrant5 <joshgg@icloud.com>
2020-11-21 10:29:54 -0800

    Create fake data seeds for participants

---
+++ db/seeds/04_participants.sql
@@ -0,0 +1,8 @@
+INSERT INTO participants (user_id, conversation_id) VALUES (1, 1);
+INSERT INTO participants (user_id, conversation_id) VALUES (2, 1);
+INSERT INTO participants (user_id, conversation_id) VALUES (2, 2);
+INSERT INTO participants (user_id, conversation_id) VALUES (3, 2);
+INSERT INTO participants (user_id, conversation_id) VALUES (3, 3);
+INSERT INTO participants (user_id, conversation_id) VALUES (4, 3);
+INSERT INTO participants (user_id, conversation_id) VALUES (4, 4);
+INSERT INTO participants (user_id, conversation_id) VALUES (1, 4);
