5e2fccde552916350b03a2c1d1529b467a8d8b3e
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-20 20:24:20 -0700

    Server Logic update On disconnect socket io logic -Filters out the disconnecting peer from a room

--- server.js
+++ server.js
@@ -28,14 +28,25 @@ io.on("connection", socket => {
 
     socket.emit("all users", usersInThisRoom);
   });
+
+  socket.on("sending signal", payload => {
+    io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+  });
+  
+  socket.on("returning signal", payload => {
+    io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+  });
+
+  socket.on('disconnect', () => {
+    const roomID = socketToRoom[socket.id];
+    let room = users[roomID];
+    if (room) {
+      room = room.filter(id => id !== socket.id);
+      users[roomID] = room;
+    }
+  });
+
 });
 
-socket.on("sending signal", payload => {
-  io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-});
-
-socket.on("returning signal", payload => {
-  io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-});
 
 server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
