ed338366836af661b51bb64bf39e989acbe6b7a6
JoshGrant5 <joshgg@icloud.com>
2020-11-21 10:29:32 -0800

    Create fake data seeds for users

---
+++ db/seeds/01_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, email, password) VALUES ('user_1', 'a@a.ca', 'password');
+INSERT INTO users (username, email, password) VALUES ('user_2', 'a@b.ca', 'password');
+INSERT INTO users (username, email, password) VALUES ('user_3', 'a@c.ca', 'password');
+INSERT INTO users (username, email, password) VALUES ('user_4', 'a@d.ca', 'password');
