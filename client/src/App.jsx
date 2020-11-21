6bda556729c6afc87230fc58310f536ec0a6cb28
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-21 16:16:19 -0700

    Create new room button and accompanying form - components: NewRoomButton, NewRoomForm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import CreateRoom from "./routes/CreateRoom";
-import Room from "./routes/Room";
+import CreateRoom from "./routes/rooms/CreateRoom";
+import Room from "./routes/rooms/Room";
+import NewRoomButton from "./routes/rooms/NewRoomButton";
 
 export default function App() {
   return (
+    <>
+     <NewRoomButton/>
      <BrowserRouter>
        <Switch>
          <Route path="/" exact component={CreateRoom} />
          <Route path="/room/:roomID" component={Room} />
        </Switch>
      </BrowserRouter>
+    </>
   );
 }
---
+++ client/src/routes/rooms/NewRoomForm.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+export default function NewRoomForm (props) {
+  const [title, setTitle] = useState(props.title || "");
+  const [description, setDescription] = useState(props.description || "");
+  const [podcastName, setPodcastName] = useState(props.podcastName || "");
+
+  const changeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const changeDescription = (event) => {
+    setDescription(event.target.value);
+  };
+
+  return (
+    <main>
+      <section className="new_room_form">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            title="title"
+            type="text"
+            placeholder="Enter Conversation Title"
+            onChange={changeTitle}
+            value={title}
+          />
+          <br/>
+          <input
+            description="description"
+            type="text"
+            placeholder="Enter Conversation Description"
+            onChange={changeDescription}
+            value={description}
+          />
+           <br/>
+          <label> Podcast </label>
+          <select>
+            <option value="Sample Pod">Sample Pod</option>
+            <option value="lime">Lime</option>
+            <option selected value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </form>
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+        </section>
+      </section>
+    </main>
+  );
+};
