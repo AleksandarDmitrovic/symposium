6ae5812d96ceadd8c214e98119c19998297ce0e1
JoshGrant5 <joshgg@icloud.com>
2020-11-21 15:09:08 -0800

    Add simple styling to TopNAV

--- client/src/routes/rooms/TopNav.jsx
+++ client/src/routes/rooms/TopNav.jsx
@@ -5,13 +5,23 @@ export default function TopNav(props) {
   // Will need creator's username as props
 
   return (
-    <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-primary">
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="/">Back</a>
-          <h4 class='nav-item'>Conversation Room</h4>
-          <h4 class='nav-item'>Timer</h4>
-        </div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-item nav-link active" href="/">Back</a>
+          </li>
+        </ul>
+      </div>
+      <div class="mx-auto order-0">
+        <h5 class="navbar-brand mx-auto">Conversation Room</h5>
+      </div>
+      <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <h5 class='mr-sm-2'>Timer</h5>
+          </li>
+        </ul>
       </div>
     </nav>
   );
