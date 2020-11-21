74986d833a749c2af439a1c8fc19ce4b9c5ac1e7
JoshGrant5 <joshgg@icloud.com>
2020-11-21 10:29:47 -0800

    Create fake data seeds for conversations

---
+++ db/seeds/03_conversations.sql
@@ -0,0 +1,11 @@
+INSERT INTO conversations (creator_id, category_id, title, description, podcast_name, podcast_starts_at, podcast_ends_at, podcast_image)
+VALUES (1, 1, 'What about this?', 'I am curious what people think about this, because I think this', 'Sample Pod', '15:36', '18:22', 'https://images.unsplash.com/photo-1478737270239-2f02b77fc618?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
+
+INSERT INTO conversations (creator_id, category_id, title, description, podcast_name, podcast_starts_at, podcast_ends_at, podcast_image)
+VALUES (2, 2, 'Or this?', 'I am curious what people think about this, because I think this', 'Sample Pod', '15:36', '18:22', 'https://images.unsplash.com/photo-1478737270239-2f02b77fc618?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
+
+INSERT INTO conversations (creator_id, category_id, title, description, podcast_name, podcast_starts_at, podcast_ends_at, podcast_image)
+VALUES (3, 3, 'Ohhhhh this too?', 'I am curious what people think about this, because I think this', 'Sample Pod', '15:36', '18:22', 'https://images.unsplash.com/photo-1478737270239-2f02b77fc618?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
+
+INSERT INTO conversations (creator_id, category_id, title, description, podcast_name, podcast_starts_at, podcast_ends_at, podcast_image)
+VALUES (4, 4, 'Gimme your thoughts?', 'I am curious what people think about this, because I think this', 'Sample Pod', '15:36', '18:22', 'https://images.unsplash.com/photo-1478737270239-2f02b77fc618?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
