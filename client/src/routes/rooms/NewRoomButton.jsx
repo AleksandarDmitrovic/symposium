1e2b18606e82c42abdf3203cf65ba963b2f94126
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-21 16:42:38 -0700

    Add modal component to newroombutton component

--- client/src/routes/rooms/NewRoomButton.jsx
+++ client/src/routes/rooms/NewRoomButton.jsx
@@ -1,21 +1,68 @@
+import { useState } from 'react';
+
 import NewRoomForm from './NewRoomForm';
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
 
-export default function Button(props) {
+function rand() {
+   return Math.round(Math.random() * 20) - 10;
+ }
+ 
+ function getModalStyle() {
+   const top = 50 + rand();
+   const left = 50 + rand();
+ 
+   return {
+     top: `${top}%`,
+     left: `${left}%`,
+     transform: `translate(-${top}%, -${left}%)`,
+   };
+ }
+ 
+ const useStyles = makeStyles((theme) => ({
+   paper: {
+     position: 'absolute',
+     width: 400,
+     backgroundColor: theme.palette.background.paper,
+     border: '2px solid #000',
+     boxShadow: theme.shadows[5],
+     padding: theme.spacing(2, 4, 3),
+   },
+ }));
+ 
 
-   const renderForm = () => {
-      //Set NewRoomForm from invisible to visible
-    }
 
-   return (
-      <>
-        <button
-           className="new_room_button"
-           onClick={renderForm}
-        >
-           Create A Conversation Room
-           {props.children}
-        </button>
-        <NewRoomForm/>
-      </>
-   );
+export default function NewRoomButton(props) {
+   const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);  
+  const handleOpen = () => {
+    setOpen(true);
+  };  
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+     <>
+       <Button color="primary"
+          className="new_room_button"
+          onClick={handleOpen}
+       >
+          Create A Conversation Room
+          {props.children}
+       </Button>
+       <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+       >
+         <NewRoomForm/>
+       </Modal>
+       
+     </>
+  );
 }
