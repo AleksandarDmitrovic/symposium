8109202f04d450d49578964f4546e30883676245
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-20 20:20:21 -0700

    Server Logic update On returning signal socket io logic -Receiving offer payload sent to other users in a room

--- server.js
+++ server.js
@@ -34,4 +34,8 @@ socket.on("sending signal", payload => {
   io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
 });
 
+socket.on("returning signal", payload => {
+  io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+});
+
 server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
