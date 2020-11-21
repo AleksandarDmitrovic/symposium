6f8cf6401f4932f8a3d1efe66d5215ba2d1eeccc
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-20 21:08:31 -0700

    Update useEffect in Room component - update accepting incoming signal of new peers

--- client/src/routes/Room.jsx
+++ client/src/routes/Room.jsx
@@ -47,7 +47,7 @@ const Room = (props) => {
     socketRef.current = io.connect("/");
     navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then(stream => {
       userVideo.current.srcObject = stream;
-      socketRef.current.emit("join room");
+      socketRef.current.emit("join room", roomID);
       socketRef.current.on("all users", users => {
         const peers = [];
         users.forEach(userID => {
@@ -57,9 +57,24 @@ const Room = (props) => {
             peer,
           })
           peers.push(peer);
-          setPeers(peers);
         })
+        setPeers(peers);
       })
+
+      socketRef.current.on("user joined", payload => {
+        const peer = addPeer(payload.signal, payload.callerID, stream);
+        peersRef.current.push({
+          peerID: payload.callerID,
+          peer,
+        })
+
+        setPeers(users => [...users, peer]);
+      });
+
+      socketRef.current.on("receiving returned signal", payload => {
+        const item = peersRef.current.find(p => p.peerID === payload.id);
+        item.peer.signal(payload.signal);
+      });
     })
   }, []);
 
@@ -73,11 +88,24 @@ const Room = (props) => {
     peer.on("signal", signal => {
       socketRef.current.emit("sending signal", {userToSignal, callerID, signal})
     })
-
+    
+    return peer;
   }
 
   function addPeer(incomingSignal, callerID, stream) {
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream,
+    })
 
+    peer.on("signal", signal => {
+      socketRef.current.emit("returning signal", {signal, callerID})
+    })
+
+    peer.signal(incomingSignal);
+
+    return peer;
   }
 
   return (
