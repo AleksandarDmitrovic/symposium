51e69d347b79efb1fed055f699f437e5a7777351
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-20 19:39:22 -0700

    Add styling to StyledVideo component in Room.jsx

--- client/src/routes/Room.jsx
+++ client/src/routes/Room.jsx
@@ -1,6 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import Peer from "simple-peer";
+import styled from "styled-components";
+
+const StyledVideo = styled.video`
+  height: 40%;
+  width: 50%;
+`;
 
 const Video = (props) => {
   const ref = useRef();
