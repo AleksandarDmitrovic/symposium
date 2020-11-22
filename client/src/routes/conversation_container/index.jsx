4f56fdd025958f6357338d2c24aafc70c60d579a
ericmcgrandle <ericmcgrandle@gmail.com>
2020-11-21 20:03:02 -0800

    add comments

--- client/src/routes/conversation_container/index.jsx
+++ client/src/routes/conversation_container/index.jsx
@@ -5,17 +5,18 @@ import axios from 'axios';
 
 export default function Conversation(props) {
 
+  // Array of all conversations returned by axios get request
   const [conversations, setConversations] = useState([]);
+
+  // String of search params from sort bar
   const [searchParam, setSearchParam] = useState('conversations')
 
   // Pass to sortby function so that it can update searchParam state
   function changeState(newState) {
-    console.log('changing state');
     setSearchParam(newState)
   };
 
   useEffect(() => {
-    console.log('useEffect', searchParam);
     axios.get(`/api/${searchParam}`).then((res) => {
       setConversations(res.data.conversation)
     })
