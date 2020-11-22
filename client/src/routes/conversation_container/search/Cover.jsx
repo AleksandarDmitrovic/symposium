aa5a7a0cde6058549d008a14a276c7c2e022313d
JoshGrant5 <joshgg@icloud.com>
2020-11-22 11:40:29 -0800

    Update css to make results responsive when hovering over

--- client/src/routes/conversation_container/search/Cover.jsx
+++ client/src/routes/conversation_container/search/Cover.jsx
@@ -1,19 +1,22 @@
 export default function Cover(props) {
 
-  // Grabs category name for creating the form
-  // console.log('Category = ', props.primaryGenreName)
+  //* Object that the searchBar will return for the creating room form
+  // const podcastInfo = {
+  //   podcast_name: props.collectionName,
+  //   podcast_image: props.artworkUrl60,
+  //   category: props.primaryGenreName
+  // }
 
-  console.log('props.state in Cover', props.state)
-
+  // Remove spaces from podcast name to provide valid route for searchParams
   const name = props.collectionName
   const term = name.split(' ').join('+');
 
   return (
-    <button onClick={() => props.state(`/conversations/podcast/${term}`)} className="podcast">
+    <article onClick={() => props.state(`/conversations/podcast/${term}`)} className="podcast">
       <img className="podcast-thumbnail" src={props.artworkUrl60} alt="Cover" />
       <div className='podcast-list'>
         <div className="podacast-result">{props.collectionName}</div>
       </div>
-    </button>
+    </article>
   );
 }
