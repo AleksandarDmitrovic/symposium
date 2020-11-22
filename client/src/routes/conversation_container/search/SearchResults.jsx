a37fb39ece48256fd22c1622562a3b314ed98dc5
JoshGrant5 <joshgg@icloud.com>
2020-11-22 15:17:21 -0800

    Add scrollbar to searchbar in form

--- client/src/routes/conversation_container/search/SearchResults.jsx
+++ client/src/routes/conversation_container/search/SearchResults.jsx
@@ -1,11 +1,14 @@
 import Cover from './Cover';
 
+import InfiniteScroll from 'react-infinite-scroller';
+
 export default function SearchResults(props) {
 
   // props.results passed down from PodcastSearch, containing podcast info from the Itunes API
   const { results } = props;
 
-  return results.map(podcast => {
+
+  const test = results.map(podcast => {
     return (
       <Cover 
         key={podcast.collectionId} 
@@ -14,6 +17,28 @@ export default function SearchResults(props) {
         changeValue = {props.changeValue}
         changePodcastInfo = {props.changePodcastInfo}
       />
-    );
+    )
   });
+  console.log('test', test)
+
+    // return (
+    //   <InfiniteScroll
+    //     pageStart={0}
+    //     // loadMore={loadFunc}
+    //     hasMore={true}
+    //     loader={<div className="loader" key={0}>Loading ...</div>}
+    //     >
+    //     <div>
+    //      {test} 
+    //     </div>
+        
+    //   </InfiniteScroll>
+    // );
+
+    return (
+      <div class='container'>
+        {test}
+      </div>
+    )
+  
 }
