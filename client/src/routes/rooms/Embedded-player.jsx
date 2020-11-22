160359f3a5ad42aefcb9efd1c64f01e76934b778
ericmcgrandle <ericmcgrandle@gmail.com>
2020-11-22 15:40:33 -0800

    working embedded. Just need collectionId as props

--- client/src/routes/rooms/Embedded-player.jsx
+++ client/src/routes/rooms/Embedded-player.jsx
@@ -1,6 +1,6 @@
 export default function EmbedVideo (props) {
 
-  const collectionID = '1441095668';
+  const collectionID = '1408856271';
   const url = `https://embed.podcasts.apple.com/us/podcast/the-joe-rogan-experience-experience/id${collectionID}`
   console.log('url', url);
 
