5be010adfe40f02bbb3375b5d1f78dc8299ad30b
JoshGrant5 <joshgg@icloud.com>
2020-11-22 19:41:00 -0800

    Add new schema and seeds for conversations to include podcast urls

--- db/schema/03_conversations.sql
+++ db/schema/03_conversations.sql
@@ -10,5 +10,7 @@ CREATE TABLE conversations (
   podcast_name VARCHAR(255) NOT NULL,
   podcast_starts_at VARCHAR(255),
   podcast_ends_at VARCHAR(255),
-  podcast_image TEXT NOT NULL
+  podcast_image TEXT NOT NULL,
+  embed_title VARCHAR(255) NOT NULL,
+  embed_url TEXT NOT NULL
 );
