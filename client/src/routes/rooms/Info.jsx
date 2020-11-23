8c3f950b3a98beda9176cc5f15c1e9d73a2917ed
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-22 17:03:59 -0700

    Add podcast image display to rooms

--- client/src/routes/rooms/Info.jsx
+++ client/src/routes/rooms/Info.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function Info(props) {
+  const altImage = "https://images.unsplash.com/photo-1556761175-129418cb2dfe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80";
 
   return (
     <div className='info'>
+      <img src={props.podcast_image} alt={altImage} />
       <ul>
         <li>Title: {props.title}</li>
         <li>Description: {props.description}</li>
