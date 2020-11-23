69afc7d02018c3f51b1234cb366890bfd829db28
JoshGrant5 <joshgg@icloud.com>
2020-11-22 19:04:49 -0800

    Added rss2json API query to fetch episode info

--- client/src/routes/conversation_container/search/PodcastSearch.jsx
+++ client/src/routes/conversation_container/search/PodcastSearch.jsx
@@ -1,36 +1,37 @@
 import { Fragment, useState, useEffect } from "react";
-
 import SearchBar from "./SearchBar";
 import SearchResults from "./SearchResults";
-
 import './searchBar.scss'
-
 const axios = require('axios');
 
 // PodcastSearch Component fetches podcast data from the Itunes API, passing down the results and the user input as props
 export default function PodcastSearch(props) {
-
   // Name to insert into Itunes API 
   const [term, setTerm] = useState("");
   // The results fetched from the API
   const [results, setResults] = useState([]);
   // The value of the selected podcast
   const [value, setValue] = useState("");
-
+  // Specific podcast
+  const [podcastName, setPodcastName] = useState();
   // Stores the setValue function to pass down as props
   const changeValue = val => {
     setValue(val);
   }
-
   useEffect(() => {
     const url = `https://itunes.apple.com/search?term=${term}&entity=podcast`;
     axios.get(url).then(response => {
-      console.log(response.data.results[0].trackViewUrl)
       setResults([...response.data.results])
+      // Make second api call for specific podcasts
+      const feedUrl = response.data.results[0].feedUrl;
+      const url =  `https://api.rss2json.com/v1/api.json?rss_url=${feedUrl}`
+      console.log('feedUrl', url);
+      axios.get(url).then(response => {
+        setPodcastName(response)
+      })
     })
     .catch(err => console.log('Error: ', err));
   }, [term])
-
   return (
     <Fragment>
       <div>
@@ -43,8 +44,6 @@ export default function PodcastSearch(props) {
           results={results}
           state={props.state}
           changeValue = {changeValue}
-          changePodcastInfo = {props.changePodcastInfo}
-          changeInput = {props.changeInput}
         />
       </div>
     </Fragment>
