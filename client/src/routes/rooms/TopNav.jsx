b880b9bfc6d50127b17994d2e70172b62d9c5dfb
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-23 22:06:24 -0700

    Implement timer component in conversation rooms

--- client/src/routes/rooms/TopNav.jsx
+++ client/src/routes/rooms/TopNav.jsx
@@ -2,31 +2,35 @@ import { useEffect, useState } from 'react';
 
 import axios from 'axios';
 
-import { Button } from '@material-ui/core';
+import Timer from 'react-compound-timer';
 
 export default function TopNav(props) {
   const [creator, setCreator] = useState('');
 
+
   useEffect(() => {
     axios.get(`/api/users/${props.creatorID}`)
-    .then(res => {
-      setCreator(res.data.user[0].username);
-    })
-    .catch(err => console.log('Error: ', err));
+      .then(res => {
+        setCreator(res.data.user[0].username);
+      })
+      .catch(err => console.log('Error: ', err));
   }, [props.creatorID]);
-
-  const back = () => {
-    props.history.push(`/`);
-    window.location.reload(false);
-  };
 
   return (
     <nav className="top-nav">
-        <ul>
-          <li><a href="/">Back</a></li>
-          <li><h5>{creator}'s Room</h5></li>
-          <li><h5>Timer</h5></li>
-        </ul>
-    </nav> 
+      <ul>
+        <li><a href="/">Back</a></li>
+        <li><h5>{creator}'s Room</h5></li>
+
+        {props.timer && 
+        <Timer>
+          <div>
+            <Timer.Hours /> :         
+            <Timer.Minutes /> : 
+            <Timer.Seconds />  
+          </div>
+        </Timer>}
+      </ul>
+    </nav>
   );
 }
