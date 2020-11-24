389a24a87848501eb28ca08e79c73ae70ac0830e
JoshGrant5 <joshgg@icloud.com>
2020-11-24 10:24:17 -0800

    Replace buttons with tabs for SortBy

--- client/src/routes/homepage/SortBy.jsx
+++ client/src/routes/homepage/SortBy.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
-import { Button, Menu, MenuItem } from '@material-ui/core';
-
-import './conversation-styles/sortby.scss'
-
+import { Button, Menu, MenuItem, Tabs, Tab } from '@material-ui/core';
 import PodcastSearch from "./search/PodcastSearch";
+import './conversation-styles/sortby.scss'
 
 export default function SortBy (props) {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -17,45 +15,103 @@ export default function SortBy (props) {
     props.state(`conversations/category/${id}`)
   };
 
-  // CATEGORY BUTTON IS HARDCODED TO 1
+  // const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  // CATEGORY BUTTON IS HARDCODED TO 1 ("OTHER")
 
   return (
-    <ul className="sort-by">
-      <li>
-        <Button 
-        aria-controls="simple-menu" 
-        aria-haspopup="true" 
-        onClick={() => { props.state('conversations') } }>
-          All 
-        </Button>
-      </li>
-      <li>
-        <div>
-          <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-            Category
-          </Button>
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={() => setAnchorEl(null)}
-          >
-            <MenuItem onClick={() => { handleClose(1)}}>Other</MenuItem>
-            <MenuItem onClick={() => { handleClose(2)}}>Technology</MenuItem>
-            <MenuItem onClick={() => { handleClose(3)}}>Arts</MenuItem>
-            <MenuItem onClick={() => { handleClose(4)}}>Business</MenuItem>
-            <MenuItem onClick={() => { handleClose(5)}}>Comedy</MenuItem>
-            <MenuItem onClick={() => { handleClose(6)}}>Education</MenuItem>
-            <MenuItem onClick={() => { handleClose(7)}}>Science</MenuItem>
-            <MenuItem onClick={() => { handleClose(8)}}>Sports</MenuItem>
-            <MenuItem onClick={() => { handleClose(9)}}>True Crime</MenuItem>
-            <MenuItem onClick={() => { handleClose(10)}}>History</MenuItem>
-            <MenuItem onClick={() => { handleClose(11)}}>Health</MenuItem>
-          </Menu>
-        </div>
-      </li>
-      <li><PodcastSearch state={props.state}/></li>
-    </ul>
+
+    <div className='sort-by-container'>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+      >
+        <Tab
+          aria-controls="simple-menu" 
+          aria-haspopup="true" 
+          onClick={() => { props.state('conversations') } }
+          label="All" 
+        />
+        <Tab 
+          aria-controls="simple-menu" 
+          aria-haspopup="true" 
+          onClick={handleClick}
+          label="Category"
+        />
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={() => setAnchorEl(null)}
+        >
+          <MenuItem onClick={() => { handleClose(1)}}>Other</MenuItem>
+          <MenuItem onClick={() => { handleClose(2)}}>Technology</MenuItem>
+          <MenuItem onClick={() => { handleClose(3)}}>Arts</MenuItem>
+          <MenuItem onClick={() => { handleClose(4)}}>Business</MenuItem>
+          <MenuItem onClick={() => { handleClose(5)}}>Comedy</MenuItem>
+          <MenuItem onClick={() => { handleClose(6)}}>Education</MenuItem>
+          <MenuItem onClick={() => { handleClose(7)}}>Science</MenuItem>
+          <MenuItem onClick={() => { handleClose(8)}}>Sports</MenuItem>
+          <MenuItem onClick={() => { handleClose(9)}}>True Crime</MenuItem>
+          <MenuItem onClick={() => { handleClose(10)}}>History</MenuItem>
+          <MenuItem onClick={() => { handleClose(11)}}>Health</MenuItem>
+        </Menu>
+      </Tabs>
+      <PodcastSearch state={props.state}/>
+    </div>
+   
+    // <ul className="sort-by">
+    //   <li>
+    //     <h5>Sort By</h5>
+    //   </li>
+    //   <li>
+    //     <Tab
+    //     aria-controls="simple-menu" 
+    //     aria-haspopup="true" 
+    //     onClick={() => { props.state('conversations') } }
+    //     label="All" 
+    //     >
+    //     </Tab>
+    //   </li>
+    //   <li>
+    //     <div>
+    //       <Tab 
+    //       aria-controls="simple-menu" 
+    //       aria-haspopup="true" 
+    //       onClick={handleClick}
+    //       label="Category"
+    //       >
+            
+    //       </Tab>
+    //       <Menu
+    //         id="simple-menu"
+    //         anchorEl={anchorEl}
+    //         keepMounted
+    //         open={Boolean(anchorEl)}
+    //         onClose={() => setAnchorEl(null)}
+    //       >
+    //         <MenuItem onClick={() => { handleClose(1)}}>Other</MenuItem>
+    //         <MenuItem onClick={() => { handleClose(2)}}>Technology</MenuItem>
+    //         <MenuItem onClick={() => { handleClose(3)}}>Arts</MenuItem>
+    //         <MenuItem onClick={() => { handleClose(4)}}>Business</MenuItem>
+    //         <MenuItem onClick={() => { handleClose(5)}}>Comedy</MenuItem>
+    //         <MenuItem onClick={() => { handleClose(6)}}>Education</MenuItem>
+    //         <MenuItem onClick={() => { handleClose(7)}}>Science</MenuItem>
+    //         <MenuItem onClick={() => { handleClose(8)}}>Sports</MenuItem>
+    //         <MenuItem onClick={() => { handleClose(9)}}>True Crime</MenuItem>
+    //         <MenuItem onClick={() => { handleClose(10)}}>History</MenuItem>
+    //         <MenuItem onClick={() => { handleClose(11)}}>Health</MenuItem>
+    //       </Menu>
+    //     </div>
+    //   </li>
+    //   <li><PodcastSearch state={props.state}/></li>
+    // </ul>
   )
 }
