5457fe703246d36636c236a2f39ffb9caa907d0c
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-24 11:35:49 -0700

    Fake chatboc component

--- client/src/routes/rooms/ChatBox.jsx
+++ client/src/routes/rooms/ChatBox.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import { Card } from '@material-ui/core';
+
 export default function ChatBox(props) {
 
   return (
-    <footer className="chat-box">
-      <h1>ChatBox</h1>
-    </footer>
+    <Card className="chat-box">
+      
+        <h1>ChatBox</h1>
+        <p>
+          fasdddddddddddddddddddddddddddddddddddddd
+          fsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          sdaf
+          sdaffsd
+          fsad
+          fasdddddddddddddddddddddddddddddddddddddd
+          fsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          sdaf
+          sdaffsd
+          fsad
+          fasdddddddddddddddddddddddddddddddddddddd
+          fsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          sdaf
+          sdaffsd
+          fsad
+        </p>
+      
+    </Card>
   );
-  }
+}
