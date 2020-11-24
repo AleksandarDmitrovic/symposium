e8edda41dc5a7534b0daab86e55b1a75bffbface
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-24 14:51:07 -0700

    Update style of conversation form

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ io.on("connection", socket => {
       room = room.filter(id => id !== socket.id);
       users[roomID] = room;
     }
-    socket.broadcast.emit('user left', socket.id)
+    socket.broadcast.emit('user left', socket.id);
   });
 
 });
