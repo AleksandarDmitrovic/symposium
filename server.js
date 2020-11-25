6d381bca08d493b590b8259855923068c83df2a2
JoshGrant5 <joshgg@icloud.com>
2020-11-25 14:22:17 -0800

    server.listen

--- server.js
+++ server.js
@@ -98,4 +98,4 @@ app.use("/api/users", usersRoutes(db));
 //   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 // });
 
-app.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
+server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
