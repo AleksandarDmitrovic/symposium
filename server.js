40f5822d265fdc85bad289599852e99abe5170ab
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-26 10:21:03 -0700

    Add logic to set NODE_ENV environment variable -For production to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const http = require("http");
 const socket = require("socket.io");
 
-// const path = require('path');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -88,7 +88,7 @@ io.on("connection", socket => {
   // When user sends a message for the chat box
   socket.on('new message', messageInfo => {
     socket.broadcast.emit('update chat box', messageInfo);
-  })
+  });
 
 });
 
@@ -100,5 +100,14 @@ const usersRoutes = require("./routes/users");
 
 app.use("/api", homepage(db));
 app.use("/api/users", usersRoutes(db));
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 server.listen(port, () => console.log(`server is running on port ${port}`));
