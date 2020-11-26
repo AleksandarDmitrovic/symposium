ffc2854f26ced3e5f7e29b17dc1cc628e95aab42
JoshGrant5 <joshgg@icloud.com>
2020-11-25 17:51:19 -0800

    comment out headers in server.js

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Proxy allowing CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://the-symposium.herokuapp.com/"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://the-symposium.herokuapp.com/"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 const users = {};
 
