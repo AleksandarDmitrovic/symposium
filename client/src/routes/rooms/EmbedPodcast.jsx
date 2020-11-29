ab5f88cb977a622df0f385273defab2c0a9bd5af
JoshGrant5 <joshgg@icloud.com>
2020-11-29 14:56:20 -0800

    Remove episode footer from podcast of day

--- client/src/routes/rooms/EmbedPodcast.jsx
+++ client/src/routes/rooms/EmbedPodcast.jsx
@@ -6,9 +6,11 @@ import 'react-h5-audio-player/src/styles.scss';
 export default function EmbedPodcast (props) {
 
   let title = props.title;
+  let episode = props.episode;
 
   if (props.class === 'footer-player') {
-    title = `Podcast of the Day: \xa0\xa0 ${props.title}`;
+    title = `Podcast of the Day: \xa0\xa0 ${props.title} \xa0\xa0\xa0 | \xa0\xa0\xa0 ${props.episode}`;
+    episode = null;
   } else if (props.class === 'convo-card-player') {
     title = `${props.title} \xa0\xa0\xa0 | \xa0\xa0\xa0 Category: ${props.category}`;
   }
@@ -22,7 +24,7 @@ export default function EmbedPodcast (props) {
         className={`embed-video-audio-player" ${props.class}`}
         src={props.embed_url}
         header={title}
-        footer={props.episode}
+        footer={episode}
         showJumpControls={false}
         customVolumeControls={volume}
         customAdditionalControls={[]}
