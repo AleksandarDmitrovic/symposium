11ceede0215f36b61ded08c577f6fdd99c9632b4
ericmcgrandle <ericmcgrandle@gmail.com>
2020-11-28 17:23:54 -0800

    refactor to use AudioPlayer

--- client/src/routes/rooms/EmbedPodcast.jsx
+++ client/src/routes/rooms/EmbedPodcast.jsx
@@ -1,22 +1,19 @@
-import { useEffect, useState, useRef } from 'react'
+import './room.scss';
+
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/src/styles.scss';
+
 
 export default function EmbedPodcast (props) {
-
-  // USE STATE
-  const [url, setUrl] = useState();
-  const myAudio = useRef();
-
-  useEffect(function() {    
-    setUrl(props.embed_url);
-    myAudio.current.load()
-  }, [props.embed_url]);
-
   return (
     <div className="embed-video">
-      <audio controls ref={myAudio}>
-        <source src={url} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
+      <h6 className="embed-video-title">{props.title}</h6>
+      <AudioPlayer
+        src={props.embed_url}
+        showJumpControls={false}
+        customAdditionalControls={[]}
+        autoPlayAfterSrcChange={false}
+      />
     </div>
   );
 }
