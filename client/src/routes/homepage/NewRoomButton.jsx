20aa1753e71a821efb131d395cce907e28a155f7
JoshGrant5 <joshgg@icloud.com>
2020-11-28 16:53:55 -0800

    Removed leftover code from merge conflict

--- client/src/routes/homepage/NewRoomButton.jsx
+++ client/src/routes/homepage/NewRoomButton.jsx
@@ -3,7 +3,7 @@ import { Button } from '@material-ui/core';
 
 import { Add } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/core/styles';
-import { Modal, Dialog, DialogContent, DialogContentText } from '@material-ui/core';
+import {  Dialog, DialogContent, DialogContentText } from '@material-ui/core';
 import './conversation-styles/index.scss';
 
 
@@ -20,16 +20,6 @@ export default function NewRoomButton(props) {
   const handleClose = () => {
     setOpen(false);
   };
-
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">Create Your Podcast Conversation</h2>
-      <NewRoomForm 
-         history = {props.history}
-         connection={props.connection}
-      />
-    </div>
-  );
 
   return (
      <>
