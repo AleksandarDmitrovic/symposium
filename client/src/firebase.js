0ee795f64b1e65b4fb16bd7ccebdc2c5da26b80e
Aleksandar <aleksandar.dmitrovic@gmail.com>
2021-02-03 10:58:05 -0700

    delete

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,13 +8,7 @@ import "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCZP19gKuZX05CQZ16Vl8cHWvwMBMlEMTo",
-  authDomain: "the-symposium.firebaseapp.com",
-  projectId: "the-symposium",
-  storageBucket: "the-symposium.appspot.com",
-  messagingSenderId: "698613168245",
-  appId: "1:698613168245:web:9de2efe826f820d92fe03c",
-  measurementId: "G-38Y4VXTPRP"
+
 };
 
 // Initialize Firebase
